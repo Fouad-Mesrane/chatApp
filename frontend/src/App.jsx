fdd5d6be1d16d024ffd0bdbc798ce87d4bba7dfe
fouad mesrane <fouadmesrane@gmail.com>
2025-03-11 10:06:02 -0600

    created pages using react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,20 @@
+import Navbar from "./components/Navbar";
+import {Route, Routes} from "react-router-dom"
 function App() {
-  return <>Hello
-  <button className="btn ">Medium</button></>;
+  return(
+
+
+    <div>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/signup" element={<SignupPage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/settings" element={<SettingsPage/>}/>
+      <Route path="/profile" element={<ProfilePage/>}/>
+    </Routes>
+    </div>
+  )
 }
 
 export default App;
