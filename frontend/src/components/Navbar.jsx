5a02568ef0a60e9eb878bdcfd22e1ca9e7e95c95
fouad mesrane <fouadmesrane@gmail.com>
2025-03-12 10:45:54 -0600

    Navbar disign finished

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { Link } from "react-router-dom";
+import { LogOut, MessageSquare, Settings, User } from "lucide-react";
 
 const Navbar = () => {
+  const { logout, authUser } = useAuthStore();
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
+      <div className="container mx-auto px-4 h-16">
+        <div className="flex justify-between items-center h-full">
+          <Link
+            to={"/"}
+            className="flex items-center gap-2 hover:opacity-80 transition-all"
+          >
+            <div className="w-9 h-9 rounded-lg bg-primary flex items-center justify-center">
+              <MessageSquare className="w-5 " />
+            </div>
+            <h1 className="text-lg font-bold">Chatly</h1>
+          </Link>
+          <div className="flex items-center gap-2">
+            <Link
+              to={"/settings"}
+              className="btn btn-sm gap-2 transition-colors"
+            >
+              <Settings className="size-4" />
+              <span className="hidden sm:inline">Settings</span>
+            </Link>
 
-export default Navbar
+            {authUser && (
+              <>
+                <Link to={"/profile"} className="btn btn-sm gap-2">
+                  <User className="size-5" />
+                  <span className="hidden sm:inline"> Profile</span>
+                </Link>
+                <button className="btn btn-sm gap-2" onClick={logout}>
+                  <LogOut className="size-5" />
+                  <span className="hidden sm:inline">Logout</span>
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
