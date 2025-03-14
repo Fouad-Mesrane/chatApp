4996526c2f1f0a286f2f6de34a1f4b3ebda60c49
fouad mesrane <fouadmesrane@gmail.com>
2025-03-14 14:01:36 -0600

    deployment configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,11 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import connectDB from "./lib/db.js";
 import cookieParser from "cookie-parser";
-
+import path from "path"
 import { app, server } from "./lib/socket.js";
+
+
+const __dirname = path.resolve()
 
 app.use(express.json());
 app.use(cookieParser());
@@ -16,8 +19,18 @@ app.use(cors(
     credentials: true,}
 ));
 
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+  app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+
+  })
+}
 
 server.listen(process.env.PORT, () => {
   console.log("Server is running on port " + process.env.PORT);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 
 // axios instance 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3001/api" : "/api",
     withCredentials: true
 })
