22e8c199f6988c9e11715e32726c8038549c5b5b
fouad mesrane <fouadmesrane@gmail.com>
2025-03-14 14:24:49 -0600

    fixed show password eye error

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -98,9 +98,9 @@ const LoginPage = () => {
                   onClick={() => setShowPassword(!showPassword)}
                 >
                   {showPassword ? (
-                    <EyeOff className="size-5 text-base-content/40" />
-                  ) : (
                     <Eye className="size-5 text-base-content/40" />
+                  ) : (
+                    <EyeOff className="size-5 text-base-content/40" />
                   )}
                 </button>
               </div>
